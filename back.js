cfabba6e15d581bd888e41a817980a32975dbb78
Joshua-Wacks <122438985+Joshua-Wacks@users.noreply.github.com>
2023-01-18 12:33:19 +0200

    fetch

--- back.js
+++ back.js
@@ -1,18 +1,21 @@
 function Login(){
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://github.com/login/oauth/authorize?scope=user:email&client_id=Iv1.622a1872fb940cbf");
+    const url = "https://github.com/login/oauth/authorize?scope=user:email&client_id=Iv1.622a1872fb940cbf"
+//     xhr.open("GET", );
     
     // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     // xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    fetch(url, { method: 'GET', headers: { 'Accept': 'application/json', }, }) 
+        .then(response => response.text()) .then(text => console.log(text))
 
-    xhr.send();
-    xhr.responseType = "json";
-    xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        const data = xhr.response;
-        console.log(data);
-    } else {
-        console.log(`Error: ${xhr.status}`);
-    }
+//     xhr.send();
+//     xhr.responseType = "json";
+//     xhr.onload = () => {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         const data = xhr.response;
+//         console.log(data);
+//     } else {
+//         console.log(`Error: ${xhr.status}`);
+//     }
 };
 }
