f1e732a225bed20eddf18bdbb8c588c20e80fe7b
daniel_haupt <danielh@bbd.co.za>
2023-01-18 11:44:00 +0200

    1

--- test/back.js
+++ test/back.js
@@ -1,6 +1,7 @@
 function githubLogin(){
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://github.com/login/oauth/authorize");
+    xhr.open("GET", "https://github.com/login/oauth/authorize?scope=user:email&client_id=Iv1.622a1872fb940cbf%");
+    
     // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     // xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With");
 
