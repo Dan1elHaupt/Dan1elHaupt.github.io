d1ccfce6a05f623a2ab556b1292efd4a75649aea
Joshua-Wacks <122438985+Joshua-Wacks@users.noreply.github.com>
2023-01-18 12:25:51 +0200

    New URL

--- back.js
+++ back.js
@@ -1,6 +1,6 @@
-function githubLogin(){
+function Login(){
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://github.com/login/oauth/authorize?scope=user:email&client_id=Iv1.622a1872fb940cbf%");
+    xhr.open("GET", "https://github.com/login/oauth/authorize?scope=user:email&client_id=Iv1.622a1872fb940cbf");
     
     // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     // xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With");
